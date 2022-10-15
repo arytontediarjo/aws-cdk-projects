c6f937917576cf282d1d9fd4929f57097f98a62d
aryton <atediarjo@gmail.com>
2022-10-15 16:37:52 +0700

    fixed git path

--- aws-cdk-pipeline/aws_cdk_pipeline/aws_cdk_pipeline_stack.py
+++ aws-cdk-pipeline/aws_cdk_pipeline/aws_cdk_pipeline_stack.py
@@ -2,15 +2,15 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 
-class MyPipelineStack(cdk.Stack):
+class AwsCdkPipelineStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         pipeline =  CodePipeline(self, "Pipeline", 
-                        pipeline_name="MyPipeline",
+                        pipeline_name="CDKPipeline",
                         synth=ShellStep("Synth", 
-                            input=CodePipelineSource.git_hub("OWNER/REPO", "main"),
+                            input=CodePipelineSource.git_hub("arytontediarjo/aws-cdk-projects", "main"),
                             commands=["npm install -g aws-cdk", 
                                 "python -m pip install -r requirements.txt", 
                                 "cdk synth"]
